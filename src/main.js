7813899bfe6a16e985369951f17dd89e95321966
Sudhanshu Gautam <me@sudhanshug.com>
2019-11-09 14:51:00 +0530

    move config to separate file and add publicPath

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
-Vue.config.configureWebpack = {
-  devtool: 'source-map'
-};
 
 new Vue({
   render: h => h(App),
